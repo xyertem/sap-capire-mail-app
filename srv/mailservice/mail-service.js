aad273872bfcaeba838ddfe5f8d084c8dcf729f0
Yuksel Ertem <yuksel.ertem@nttdata.com>
2024-11-10 19:48:55 +0000

    mail service mdf

--- srv/mailservice/mail-service.js
+++ srv/mailservice/mail-service.js
@@ -8,9 +8,9 @@ const { Mail } = require('./mail');
 module.exports = cds.service.impl(async function () {
     this.on('triggerMailEvent', async ( req, res ) => {
         try {
-            const mail = new Mail('Sending with SendGrid is Fun',null,mailbody,'ykselertem@yandex.com');
+            const mail = new Mail('Sending with SendGrid is Fun',mailbody,null,'ykselertem@yandex.com');
             await mail.sendMail();
-            return req.notify(200)
+            return req.notify(200);
         } catch (error) {
             console.log(error);
             throw error;
